327cc4dcb95b0768c9879e52bd38feb20e44335d
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-02-07 22:33:43 +0000

    google maps added to countries show page

---
+++ app/assets/javascripts/countries.js
@@ -0,0 +1,37 @@
+
+// function myMap() {
+//   var lat; 
+//   var lng;
+//   var markers = [];
+
+//   var mapProp = {
+//       center: new google.maps.LatLng(22,98),
+//       zoom: 4,
+//       mapTypeId: google.maps.MapTypeId.HYBRID
+//   };
+
+//   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+//   var countries = <%= @countries.to_json.html_safe %>; 
+
+//   countries.forEach(function (country) {
+//     placeMarker({id: country.id, lat: country.latitude, lng: country.longitude});
+//   });
+
+//   function placeMarker(latLng) {
+//     var marker = new google.maps.Marker({
+//       id: latLng.id,
+//       position: latLng,
+//       map: map,
+//       draggable: false,
+//       animation: google.maps.Animation.DROP
+//     });
+//     google.maps.event.addListener(marker, 'click', function() { 
+//        console.log("I am marker " + marker.id); 
+//        location.href = `/countries/${marker.id}`;
+//     }); 
+//     return marker; 
+//     markers.push(latLng);
+//   }
+
+}
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,12 @@
 class CountriesController < ApplicationController
+
+  def index 
+    @countries = Country.all 
+  end 
+
+  def show
+    @country = Country.find_by(id: params[:id])
+    @artefacts = @country.artefacts.sort_by{|artefact| artefact[:id]}
+  end 
+
 end
