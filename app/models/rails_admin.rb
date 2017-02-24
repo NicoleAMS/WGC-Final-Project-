b1d3d07376334ed0c102b0fad4610d2d711f2b74
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-02-24 22:52:13 +0000

    commented out RailsAdmin class

--- app/models/rails_admin.rb
+++ app/models/rails_admin.rb
@@ -1,4 +1,4 @@
-class RailsAdmin < RailsAdmin::Engine
+# class RailsAdmin < RailsAdmin::Engine
 
   # added this model myself to avoid an error cause RailsAdmin isn't recognised as a class (it still isn't in the test)
-end 
+# end 
