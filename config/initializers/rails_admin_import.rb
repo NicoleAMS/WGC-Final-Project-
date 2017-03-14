5ec1b2953010d3532b5b7c28a6208ada1554a8f4
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-03-14 15:29:55 +0000

    updated import

--- config/initializers/rails_admin_import.rb
+++ config/initializers/rails_admin_import.rb
@@ -2,10 +2,10 @@ RailsAdmin.config do |config|
   # REQUIRED:
   # Include the import action
   # See https://github.com/sferik/rails_admin/wiki/Actions
-  config.actions do
-    all
-    import
-  end
+  # config.actions do
+  #   all
+  #   import
+  # end
 
   # Optional:
   # Configure global RailsAdminImport options
